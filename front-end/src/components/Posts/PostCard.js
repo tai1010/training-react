9f9774714e42021947e531828d3b30185d5067cc
tai <tai.8989@icloud.com>
2022-03-27 22:44:19 +0900

    画面描画

--- front-end/src/components/Posts/PostCard.js
+++ front-end/src/components/Posts/PostCard.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 export default function PostCard(props) {
   const { name, desc } = props;
   return (
-    <div className='col-3 mb-4'>
+    <div className="col-3 mb-4">
       <Card>
         <CardHeader
           avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">R</Avatar>
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
           }
           action={
             <IconButton aria-label="settings">
--- front-end/src/components/Posts/PostList.js
+++ front-end/src/components/Posts/PostList.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import React from "react";
+import { useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import Skeleton from "@mui/material/Skeleton";
-import './post.scss';
-import PostCard from './PostCard';
+import "./post.scss";
+import PostCard from "./PostCard";
 
 export default function PostList() {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(false);
 
-  const [data] = useState(Array(12).fill({
-    name: 'Shrimp and Chorizo Paella',
-    desc: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.'
-  }));
+  const [data] = useState(
+    Array(12).fill({
+      name: "Shrimp and Chorizo Paella",
+      desc: "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+    })
+  );
 
   function processLoading() {
     return Array.from(new Array(12)).map((item, index) => (
-      <div className='col-3 mb-5' key={index}>
+      <div className="col-3 mb-5" key={index}>
         <Skeleton variant="rectangular" height={150} />
-        <div className='pt-3'>
+        <div className="pt-3">
           <Skeleton />
           <Skeleton width="60%" />
         </div>
@@ -27,12 +29,18 @@ export default function PostList() {
   }
 
   return (
-    <div className='post-list'>
-      <div className='row'>
-        {isLoading ? processLoading() : data.map((item, idx) => <PostCard {...item} key={idx} />)}
+    <div className="post-list">
+      <div className="row">
+        {isLoading
+          ? processLoading()
+          : data.map((item, idx) => <PostCard {...item} key={idx} />)}
       </div>
       <Stack spacing={2} direction="row" justifyContent="flex-end">
-        {isLoading ? <Skeleton variant="text" width={200} /> : <Pagination count={10} />}
+        {isLoading ? (
+          <Skeleton variant="text" width={200} />
+        ) : (
+          <Pagination count={10} />
+        )}
       </Stack>
     </div>
   );
